3964b0f842e3675c9e208d3885ca67efd8321b95
EduNunes52 <carloseduardo.nunes.52@outlook.com>
2025-04-02 19:47:25 -0300

    Ajuste na classe BookController

--- book-backend/Book/src/main/java/com/example/Book/Controller/BookController.java
+++ book-backend/Book/src/main/java/com/example/Book/Controller/BookController.java
@@ -1,28 +1,24 @@
 package com.example.Book.Controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.Book.Model.Book;
 import com.example.Book.Repository.BookRepository;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping("/api/books")
+@CrossOrigin(origins = "*")
 public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
-
     @GetMapping
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
-
     @PostMapping
     public Book createBook(@RequestBody Book book) {
         return bookRepository.save(book);
     }
-
 }
